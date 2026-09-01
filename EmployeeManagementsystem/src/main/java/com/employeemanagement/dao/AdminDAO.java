2dc87b42fcbdcfdd07699a9e303e6a5c7ce64b14
Gowtham162005 <nagarajgowtham54@gmail.com>
2026-09-01 17:55:00 +0530

    admin updation

--- EmployeeManagementsystem/src/main/java/com/employeemanagement/dao/AdminDAO.java
+++ EmployeeManagementsystem/src/main/java/com/employeemanagement/dao/AdminDAO.java
@@ -11,24 +11,39 @@ public class AdminDAO {
 
     public boolean login(String username, String password) {
 
-        String sql =
-            "SELECT * FROM admin WHERE username = ? AND password = ?";
+        String sql = "SELECT username, password FROM admin WHERE username = ?";
 
         try {
+            Connection connection = DBConnection.getConnection();
 
-            Connection connection =
-                    DBConnection.getConnection();
+            if (connection == null) {
+                System.out.println("ERROR: Database connection is NULL");
+                return false;
+            }
 
-            PreparedStatement ps =
-                    connection.prepareStatement(sql);
+            PreparedStatement ps = connection.prepareStatement(sql);
 
             ps.setString(1, username);
-            ps.setString(2, password);
 
             ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
-                return true;
+
+                String dbUsername = rs.getString("username");
+                String dbPassword = rs.getString("password");
+
+                System.out.println("Entered username: [" + username + "]");
+                System.out.println("Database username: [" + dbUsername + "]");
+
+                if (password != null && password.equals(dbPassword)) {
+                    System.out.println("PASSWORD MATCH!");
+                    return true;
+                } else {
+                    System.out.println("PASSWORD DOES NOT MATCH!");
+                }
+
+            } else {
+                System.out.println("USERNAME NOT FOUND!");
             }
 
         } catch (SQLException e) {
